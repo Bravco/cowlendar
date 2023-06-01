c2e40dc98cee5efbb8c118792bab08bb3be4c10d
Bravco <boss.wiw@gmail.com>
2023-06-01 07:37:03 +0200

    Updated base

--- web/vite.config.js
+++ web/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/cowlendar/",
+  base: "/web/",
   plugins: [vue()],
   resolve: {
     alias: {
